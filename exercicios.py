9feb1e16ce729ceb582fbd8304c191e42a950d26
creep <radiohead@gmail.com>
2024-08-27 22:24:54 -0300

    aula inicial

---
+++ exercicios.py
@@ -0,0 +1,19 @@
+# def saudacao():
+#     nome=input("qual e seu nome? ")
+#     print("ola" +nome)
+    
+# saudacao()
+
+# def soma_produto():
+#     n1 = int(input("Coloque o primeiro número: "))
+#     n2 = int(input("Coloque o segundo número: "))
+    
+#     print(n1+n2)
+    
+# soma_produto()
+
+def err():
+    n2 = int(input("Coloque o segundo número: "))
+
+
+print(n2)
